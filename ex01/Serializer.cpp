6fbc729cf17c4c2923f2f60083082b5fdfeda2f9
mmad <mohamed.mad1-etu@etu.univh2c.ma>
2024-12-18 11:29:30 +0100

    done

--- ex01/Serializer.cpp
+++ ex01/Serializer.cpp
@@ -1,7 +1,7 @@
 #include "Serializer.hpp"
 
 Serializer::Serializer(){
-    // std::cout << "[Serializer] Default constructor is called" << std::endl;
+    std::cout << "[Serializer] Default constructor is called" << std::endl;
 }
 
 Serializer::Serializer(const Serializer& s){
@@ -18,16 +18,35 @@ Serializer& Serializer::operator=(const Serializer& other){
 }
 
 Serializer::~Serializer(){
-    // std::cout << "[Serializer] Distructor is called" << std::endl;
+    std::cout << "[Serializer] Distructor is called" << std::endl;
 }
 
 uintptr_t Serializer::serialize(Data* ptr){
-    uintptr_t data = reinterpret_cast<std::uintptr_t>(ptr->ptr_t);
-    return data;
+    // if (ptr->cptr_t)
+    //     return reinterpret_cast<std::uintptr_t>(&ptr->cptr_t);
+    if(ptr->ptr_t){
+        // uintptr_t ptrInteger = reinterpret_cast<std::uintptr_t>(&ptr->ptr_t);
+        // ptrInteger += 0;
+        // int *data = reinterpret_cast<int *>(ptrInteger);
+        // std::cout << "Example : " << *data << std::endl;
+        return reinterpret_cast<std::uintptr_t>(ptr->ptr_t);
+    }
+    return 0;
 }
 
 Data* Serializer::deserialize(uintptr_t raw){
     Data *data = new Data;
+    // std::cout << raw << std::endl;
+    // if (data->ptr_t != NULL){
+    //     data->ptr_t = reinterpret_cast<int *>(raw);
+    //     return data;
+    // }
+    // if (data->cptr_t != NULL){
+    //     data->cptr_t = reinterpret_cast<char *>(raw);
+    //     return data;
+    // }
+    // data->cptr_t = reinterpret_cast<char *>(raw);
     data->ptr_t = reinterpret_cast<int *>(raw);
+    
     return data;
 }
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,34 +1,64 @@
 #include "Serializer.hpp"
 #include <cstdint>
 
+struct s_main
+{
+    int height;
+    int width;
+};
+
 int main(int argc, char **argv){
     
     if (argc < 2 ){
         std::cout << "<convert> put some data to convert" << std::endl;
         return EXIT_FAILURE;
     }
-    Serializer s;
-    Data* data = new Data;
-    int data_struct = atoi(argv[1]);
-    int data_test = data_struct * 0;
-    data->ptr_t = &data_struct;
-    // data->cptr_t = strdup("this is just a test...");
-    uintptr_t data_t = s.serialize(data);
-    // unsigned int n1 = __INT_MAX__;
-    // unsigned long n2 = __INT32_MAX__;
-    Data* test_data = new Data;
-    test_data->ptr_t = &data_test;
-    data_t = s.serialize(data); 
 
-    // std::cout << data_t << std::endl;
-    // return EXIT_FAILURE;
-    // char *desrialize_data = reinterpret_cast<char *>(data);
-    Data *new_Data = new Data;
-    new_Data = s.deserialize(data_t);
+    {
+        s_main struct_data = {100, 200};
+        char buffer_first[sizeof(struct_data)];
+        memcpy(buffer_first, &struct_data, sizeof(struct_data));
+        std::cout << *((int *)buffer_first) << std::endl;
+        
+        uintptr_t holder = reinterpret_cast<uintptr_t>(&buffer_first + 1);
+        // holder += 0;
+        std::cout << *reinterpret_cast<int *>(holder) << std::endl;
+        return 0;
+        Serializer s;
+        Data* data = new Data {};
+        int input = atoi(argv[1]);
+        data->ptr_t = &input;
+        data->cptr_t = strdup("this is just a test");
+        char buffer[sizeof(data)];
+        std::cout << sizeof(data) << std::endl;
+        memcpy(buffer, &data, sizeof(data));
+        std::cout << *((int *)buffer)<< std::endl;
+        std::cout << "Output : " << data->cptr_t << std::endl;
+        
+        uintptr_t result = s.serialize(data);
+        data = s.deserialize(result);
+        std::cout << "Output : " << *data->ptr_t << std::endl;
+        return (EXIT_SUCCESS);
+    }
 
-    // char *desrialize_data2 = reinterpret_cast<char *>(data);
-    // std::cout << new_Data->cptr_t << std::endl;
-    std::cout << *new_Data->ptr_t << std::endl;
+    {
+        Serializer s;
+        Data* data = new Data;
+        int input = atoi(argv[1]);
+        data->ptr_t = &input;
+        uintptr_t result = s.serialize(data);
+        data = s.deserialize(result);
+        std::cout << "Output : " << *data->ptr_t << std::endl;
+        return EXIT_SUCCESS;
+    }
+    std::cout << "-----------------------------" << std::endl;
 
-    return EXIT_SUCCESS;
+    {
+        Serializer s;
+        Data* data = new Data;
+        data->cptr_t = strdup(argv[1]);
+        data = s.deserialize(s.serialize(data));
+        std::cout << "Output : " << data->cptr_t << std::endl;
+        return EXIT_SUCCESS;
+    }
 }
