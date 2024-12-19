4932b32b2d252258b5d264bc1b5db2fcfa01b0fc
mmad <mohamed.mad1-etu@etu.univh2c.ma>
2024-12-19 15:04:03 +0100

    Updated

--- ex02/Base.cpp
+++ ex02/Base.cpp
@@ -9,15 +9,6 @@ Base::~Base()
     std::cout << "[Base] Distructor is called" << std::endl;
 }
 
-int hashit(std::string name_form)
-{
-    std::string arr[4] = {"A", "B", "C", ""};
-    int i = 0;
-    while (name_form != arr[i] && !arr[i].empty())
-        i++;
-    return i + 1;
-}
-
 int returnNumber()
 {
     int number = 0;
@@ -29,56 +20,27 @@ int returnNumber()
 base-to-derived conversions are not allowed
 with dynamic_cast unless the base class is polymorphic.
  */
+class Test : public Base{};
 Base *Base::generate()
 {
-    Base *_Base = new Base;
     int number = returnNumber();
     if (number == 1)
     {
-        try
-        {
-            if (dynamic_cast<Base *>(new A))
-            {
-                std::cout << "FLAG 1" << std::endl;
-                return new Base;
-            }
-        }
-        catch (const std::bad_cast &e)
-        {
-            std::cerr << e.what() << '\n';
-        }
+    //    Base *_Base = dynamic_cast<Base*>(new A);
+       Base *_Base = dynamic_cast<Base*>(new Test);
+       return _Base;
+        
     }
 
     if (number == 2)
     {
-        try
-        {
-            if (dynamic_cast<Base *>(new B))
-            {
-                std::cout << "FLAG 2" << std::endl;
-                return new Base;
-            }
-        }
-        catch (const std::bad_cast &e)
-        {
-            std::cerr << e.what() << '\n';
-        }
+        Base *_Base = dynamic_cast<Base*>(new B);
+        return _Base;
     }
     if (number == 3)
     {
-        C *D = new C;
-        try
-        {
-            if (dynamic_cast<Base *>(new C))
-            {
-                std::cout << "FLAG 3" << std::endl;
-                return new Base;
-            }
-        }
-        catch (const std::bad_cast &e)
-        {
-            std::cerr << e.what() << '\n';
-        }
+        Base *_Base = dynamic_cast<Base*>(new C);
+        return _Base;
     }
     return NULL;
 }
@@ -87,23 +49,12 @@ void Base::identify(Base *p)
 {
     if (dynamic_cast<A *>(p))
         dynamic_cast<A *>(p)->function();
-    else{
-        std::cout << "ERROR A" << std::endl;
-        return;
-    }
-
-    if (dynamic_cast<B *>(p))
+    else if (dynamic_cast<B *>(p))
         dynamic_cast<B *>(p)->function();
-    else{
-        std::cout << "ERROR B" << std::endl;
-        return ;
-    }
-    if (dynamic_cast<C *>(p))
+    else if (dynamic_cast<C *>(p))
         dynamic_cast<C *>(p)->function();
-    else{
-        std::cout << "ERROR C" << std::endl;
-        return;
-    }
+    else
+        std::cout << "ERROR" << std::endl;
 }
 
 void Base::identify(Base &p)
@@ -112,88 +63,3 @@ void Base::identify(Base &p)
     // static_cast<B>(p).
 }
 
-/*
-Base *Base::generate()
-{
-    Base *_Base = new Base;
-    // if (dynamic_cast<Base *>(new A)){
-    // if (dynamic_cast<A*>(_Base)){
-    //     std::cout << "check it's allowed" << std::endl;
-    //     return _Base;
-    // }
-    // else{
-    //     std::cout << "ERROR" << std::endl;
-    // }
-    // return NULL;
-    int number = returnNumber();
-    if (number == 1)
-    {
-        A *D = new A;
-        try
-        {
-            if (dynamic_cast<Base *>(new A))
-            {
-                std::cout << "I was here" << std::endl;
-                return new Base;
-            }
-
-            Base *returnBase = dynamic_cast<A *>(_Base);
-            std::cout << "I was here 1" << std::endl;
-            return returnBase;
-        }
-        catch (const std::exception &e)
-        {
-            std::cerr << e.what() << '\n';
-        }
-    }
-    else if (number == 2)
-    {
-        B *D = new B;
-        try
-        {
-            Base *returnBase = dynamic_cast<B *>(_Base);
-            std::cout << "I was here 2" << std::endl;
-            return returnBase;
-        }
-        catch (const std::exception &e)
-        {
-            std::cerr << e.what() << '\n';
-        }
-    }
-    else
-    {
-        C *D = new C;
-        try
-        {
-            Base *returnBase = dynamic_cast<C *>(_Base);
-            std::cout << "I was here 3" << std::endl;
-            return returnBase;
-        }
-        catch (const std::exception &e)
-        {
-            std::cerr << e.what() << '\n';
-        }
-    }
-    return NULL;
-}
-*/
-
-/*
-
-void Base::identify(Base *p)
-{
-    if (p)
-    {
-        int number = returnNumber();
-        if (number == 1)
-        {
-            // dynamic_cast<void *>(p)->f
-            dynamic_cast<A *>(p)->function();
-        }
-        else if (number == 2)
-            dynamic_cast<B *>(p)->function();
-        else
-            dynamic_cast<C *>(p)->function();
-    }
-}
-*/
